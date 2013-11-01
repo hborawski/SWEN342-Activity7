9be2830be111175290d1b814d98216403ea44436
Harris Borawski <harrisborawski@me.com>
2013-11-01 13:36:41 -0400

    skeleton for scanactor

--- src/ScanActor.java
+++ src/ScanActor.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
-public class ScanActor {
+import akka.actor.UntypedActor;
+
+
+public class ScanActor extends UntypedActor {
+	private ArrayList<String> lines;
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void readFile(String fileName){
+		//read file and store lines in ArrayList
+	}
+	
+	public void sendMessage(){
+		//construct and send found object to CollectionActor
+	}
 
 }
