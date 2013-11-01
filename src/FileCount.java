d8241693f9e09bc29993bf07bd2f50efe4f0d815
Harris Borawski <harrisborawski@me.com>
2013-11-01 13:25:00 -0400

    filecount object

--- src/FileCount.java
+++ src/FileCount.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
 public class FileCount {
-
+	private int count;
+	private ArrayList<String> filenames;
+	
+	public FileCount(int count, ArrayList<String> filenames){
+		this.count = count;
+		this.filenames = filenames;
+	}
+	
+	public int getCount(){
+		return count;
+	}
+	
+	public ArrayList<String> getFilenames(){
+		return filenames;
+	}
 }
