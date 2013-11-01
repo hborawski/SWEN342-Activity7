d0d36d13da0c0e0119c1c52733ca268136353719
Harris Borawski <harrisborawski@me.com>
2013-11-01 13:21:33 -0400

    Found object

--- src/Found.java
+++ src/Found.java
@@ -1,4 +1,18 @@
-
+import java.util.ArrayList;
 public class Found {
-
+	private String filename;
+	private ArrayList<String> lines;
+	
+	public Found(String filename, ArrayList<String> lines){
+		this.filename = filename;
+		this.lines = lines;	
+	}
+	
+	public String getFilename(){
+		return filename;
+	}
+	
+	public ArrayList<String> getLines(){
+		return lines;
+	}
 }
