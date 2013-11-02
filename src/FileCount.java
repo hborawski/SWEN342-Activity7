5bc90680a2c0ca9c846d8964451346f53d836050
skuloth <skuloth@gmail.com>
2013-11-02 16:38:13 -0400

    Added pattern to filecount

--- src/FileCount.java
+++ src/FileCount.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class FileCount {
 	private int count;
 	private ArrayList<String> filenames;
+	private String pattern;
 	
-	public FileCount(int count, ArrayList<String> filenames){
+	public FileCount(int count, ArrayList<String> filenames, String pattern){
 		this.count = count;
 		this.filenames = filenames;
+		this.pattern = pattern;
 	}
 	
 	public int getCount(){
