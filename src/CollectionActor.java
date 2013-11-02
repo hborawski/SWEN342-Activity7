06effe393c523774372714540bf126252917d532
skuloth <skuloth@gmail.com>
2013-11-02 16:58:29 -0400

    Update CollectionActor.java

--- src/CollectionActor.java
+++ src/CollectionActor.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import akka.actor.UntypedActor;
 
 public class CollectionActor extends UntypedActor {
-		private ArrayList<ScanActor> actors;
+		private ArrayList<ActorRef> actors;
 		private int messages;
 		private int count;
 		
@@ -17,19 +17,20 @@ public class CollectionActor extends UntypedActor {
                 		FileCount fc = (FileCount)arg0;
                 		count = fc.getCount();
                         for(int i = 0; i < count; i++){
-                        	actors.add(new ScanActor());
-                        	actors.get(i).tell(new Configure( fc.filenames[i], fc.pattern, this));                       	
+                        	ActorRef actor = new ScanActor();
+                        	actors.add(actor);
+                        	actors.get(i).tell(new Configure( fc.filenames.get(i), fc.pattern, this));                       	
                         }
                         
                 }else if(arg0 instanceof Found){
                         //print found objects
-                        Found found = (Found)arg1;
+                        Found found = (Found)arg0;
                         System.out.println(found.getFilename());
                         for(String s : found.getLines()){
                                 System.out.println(s);
                         }
                         messages++;
-                        if(messages = count){
+                        if(messages == count){
                         	Actors.registry().shutdown();
                         }
                 }
